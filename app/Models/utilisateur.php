054af10fb9885c588e8d8544f627d94c221fa4ef
Madery Romain <72389130+Exarilo@users.noreply.github.com>
2021-03-01 16:49:45 +0100

    Les koalas c'est cool
    vive leur migration

---
+++ app/Models/utilisateur.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class utilisateur extends Model
+{
+    use HasFactory;
+    public function scopeIsAdmin($query)
+    {
+        $query->where('is_admin', true);
+    }
+}
--- database/migrations/2021_03_01_142144_utilisateur.php
+++ database/migrations/2021_03_01_142144_utilisateur.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Utilisateur extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('utilisateur', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->integer('tel');
+            $table->string('adresse');
+            $table->boolean('estAdmin');
+            $table->integer('placeDansLaListeAttente');
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('utilisateur');
     }
 }
